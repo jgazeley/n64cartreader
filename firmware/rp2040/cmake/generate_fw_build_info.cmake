d65985ac5adec23331316058775e74285edd9bc4
Jayson Gazeley <jayson.gazeley@gmail.com>
2026-04-11 15:51:20 -0700

    Fix firmware build version tag

--- firmware/rp2040/cmake/generate_fw_build_info.cmake
+++ firmware/rp2040/cmake/generate_fw_build_info.cmake
@@ -14,13 +14,24 @@ endif()
 set(FW_GIT_DESCRIBE "nogit")
 if(DEFINED GIT_EXECUTABLE AND NOT GIT_EXECUTABLE STREQUAL "" AND EXISTS "${GIT_EXECUTABLE}")
   execute_process(
-    COMMAND "${GIT_EXECUTABLE}" -C "${SOURCE_DIR}" describe --always --dirty --tags
+    COMMAND "${GIT_EXECUTABLE}" -C "${SOURCE_DIR}" rev-parse --short=7 HEAD
     OUTPUT_VARIABLE FW_GIT_DESCRIBE
+    RESULT_VARIABLE FW_GIT_RESULT
     OUTPUT_STRIP_TRAILING_WHITESPACE
     ERROR_QUIET
   )
-  if(FW_GIT_DESCRIBE STREQUAL "")
+  if(NOT FW_GIT_RESULT EQUAL 0 OR FW_GIT_DESCRIBE STREQUAL "")
     set(FW_GIT_DESCRIBE "nogit")
+  else()
+    execute_process(
+      COMMAND "${GIT_EXECUTABLE}" -C "${SOURCE_DIR}" diff --quiet --ignore-submodules HEAD --
+      RESULT_VARIABLE FW_GIT_DIRTY_RESULT
+      OUTPUT_QUIET
+      ERROR_QUIET
+    )
+    if(FW_GIT_DIRTY_RESULT EQUAL 1)
+      string(APPEND FW_GIT_DESCRIBE "-dirty")
+    endif()
   endif()
 endif()
 
